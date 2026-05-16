10ee98a2c8a43b1e8eb58d2d4144cc7f9bf55e42
simonys32 <91671425+simonys32@users.noreply.github.com>
2026-05-17 01:32:50 +0200

    Update ToDoTest.java

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoTest.java
@@ -1,23 +1,22 @@
 package ch.zhaw.iwi.devops.demo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+  import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+  import org.junit.jupiter.api.Test;
 
-public class ToDoTest {
+  public class ToDoTest {
 
-    @Test
-    public void testToDo() {
-        var toDo1 = new ToDo(1, "title", "description");
-        assertEquals("title", toDo1.getTitle());
-        assertEquals("description", toDo1.getDescription());
-        assertEquals(1, toDo1.getId());
-    }
-     @Test
-    void testToDoDescription() {
-      ToDo todo = new ToDo();
-      todo.setDescription("Test description");
-      assertEquals("Test description", todo.getDescription());
+      @Test
+      public void testToDo() {
+          var toDo1 = new ToDo(1, "title", "description");
+          assertEquals("title", toDo1.getTitle());
+          assertEquals("description", toDo1.getDescription());
+          assertEquals(1, toDo1.getId());
+      }
+
+      @Test
+      void testToDoTitle() {
+          ToDo todo = new ToDo(2, "new title", "new description");
+          assertEquals("new title", todo.getTitle());
+      }
   }
-    
-}
